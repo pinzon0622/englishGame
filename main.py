9ec928d9b78bf3b4ae46fbaa97bfd86a745fb884
Mateo Pinzon <pinzon0622@gmail.com>
2024-03-16 17:16:26 -0500

    Estructura basica para trabajar

---
+++ main.py
@@ -0,0 +1,7 @@
+import os
+import tkinter as tk
+from tkinter import ttk, messagebox
+from questionsData import questions
+
+
+
---
+++ questionsData.py
@@ -0,0 +1,5 @@
+questions = [{
+    "question": "What is the capital of France?",
+    "options": ["Paris", "London", "Berlin", "Madrid"],
+    "answer": "Paris"
+}]
